ab20adbdd8e6c4f1034a75d303e6259e161dca02
Tarso Jabbes <tarsojabbes@gmail.com>
2023-05-07 11:43:37 -0300

    Inclui configuração para CORS, atualização do README e query parameter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Teste técnico - ImpulsoGov - Backend
 
-Esta API utiliza do Poetry para gerenciamento de dependências. Portanto, para executá-la é necessário ter o Poetry instalado. Você pode saber como por [aqui](https://python-poetry.org/docs/)
+Esta API utiliza do Poetry para gerenciamento de dependências. Portanto, para executá-la é necessário ter o Poetry instalado. Você pode saber como por [aqui](https://python-poetry.org/docs/).
 
 ## Como rodar a API
 
@@ -22,3 +22,8 @@ Esta API utiliza do Poetry para gerenciamento de dependências. Portanto, para e
     ```
 Com isso, a API estará rodando na porta 8000 do seu localhost. Você pode acessar a API em seu navegador da web visitando o endereço http://localhost:8000.
 
+## Rotas e Parâmetros
+
+- A rota <code>localhost:8000</code> retornará ambos os horários atuais em GMT -3 e GMT -5, caso seja informado a query parameter <code>gmt</code> (ex: <code>localhost:8000/?gmt=-3</code>), será retornado o GMT somente para aquele fuso horário.
+
+- Caso o GMT informado seja diferente de -3 e -5, a API retornará um erro 400 (Bad Request) e uma mensagem de erro informando que não o GMT informado não é suportado.
--- main.py
+++ main.py
@@ -1,17 +1,48 @@
-from typing import List, Dict
-from fastapi import FastAPI
+from typing import Dict
 from datetime import datetime, timedelta
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Adiciona middleware para permitir CORS
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_current_time_gmt_minus3_minus5() -> tuple[str, str]:
+    """Retorna a hora atual em GMT-3 e GMT-5 no formato HH:MM:SS"""
+    current_utc = datetime.utcnow()
+    current_time_gmt_minus3 = (current_utc - timedelta(hours=3)).strftime("%H:%M:%S")
+    current_time_gmt_minus5 = (current_utc - timedelta(hours=5)).strftime("%H:%M:%S")
+    return (current_time_gmt_minus3, current_time_gmt_minus5)
+
 @app.get("/")
-async def current_time() -> List[Dict[str, str]]:
+async def current_time(gmt: int = None) -> Dict[str, str]:
     """
     Endpoint que retorna a hora atual em GMT-3 e GMT-5.
-    """
-    current_utc = datetime.utcnow()
-    current_time_gmt_minus_3 = (current_utc - timedelta(hours=3)).strftime("%H:%M:%S")
-    current_time_gmt_minus_5 = (current_utc - timedelta(hours=5)).strftime("%H:%M:%S")
 
-    return [{"Current time at GMT -3": current_time_gmt_minus_3,
-             "Current time at GMT -5": current_time_gmt_minus_5}]
+    Parâmetros:
+    - gmt (int): GMT desejado. Se não informado, retorna ambos os horários.
+    - Retorno:
+      - Current time at GMT -3 (str): horário atual em GMT-3 no formato HH:MM:SS.
+      - Current time at GMT -5 (str): horário atual em GMT-5 no formato HH:MM:SS.
+    """
+    current_time_gmt_minus3, current_time_gmt_minus5 = get_current_time_gmt_minus3_minus5()
+
+    if gmt == -3: 
+        return {"Current time at GMT -3": current_time_gmt_minus3}
+    
+    if gmt == -5:
+        return {"Current time at GMT -5": current_time_gmt_minus5}
+    
+    if gmt is None:
+        return {"Current time at GMT -3": current_time_gmt_minus3,
+             "Current time at GMT -5": current_time_gmt_minus5}
+    
+    raise HTTPException(status_code=400, detail=f"GMT {gmt} not supported")
